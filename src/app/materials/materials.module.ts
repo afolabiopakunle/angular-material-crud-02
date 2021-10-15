381cc8253437e76214ad2856c8a927f596b90cc8
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-15 09:55:10 +0100

    abandoned

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion'; 
+
 const MaterialComponents = [
-  
+  MatExpansionModule
 ]
 @NgModule({
   imports: [
-    
+    MaterialComponents
   ],
+  exports: [MaterialComponents]
 })
 export class MaterialsModule { }
